5c871c9f77dfed8e6db48cab9ec650b8e4c53f28
Corbin <corbin@worldedi.com>
2022-02-02 20:28:00 -0500

    first html page

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("bbbutton").onclick = function () {
+       location.href = "file:///C:/Users/Owner/Documents/githubsite/corbincaustin.github.io/index.html#bb";
+   };
+
+document.getElementById("ccbutton").onclick = function () {
+      location.href = "file:///C:/Users/Owner/Documents/githubsite/corbincaustin.github.io/index.html#cc";
+    };
